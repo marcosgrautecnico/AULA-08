f0fdf18763aeda636215b750a67323dbb408cd38
marcosgrautecnico <165164535+marcosgrautecnico@users.noreply.github.com>
2024-04-08 11:20:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+window.alert('Bem vindo ao Questionario seduzente')
+window.confirm('Esta grosso ?')
+window.prompt('Quantos cm ?')
+var nome = window.prompt('Qual seu nome ?')
+window.alert('Seja bem vindo ' + nome + '!')
+
+var n1 = window.prompt('Digite um numero')
+var n1 = window.prompt('Digite outro numero')
+var s = n1 + n2
+var s = window.alert('A soma dos 2 numeros é ' + s)
+
+var n1 = Number.parseInt(window.prompt('Digie um numero'))
+var n2 = Number.parseInt(window.prompt('Digite outro numero'))
+var s = n1 + n2
+var s = window.alert('A soma dos 2 numeros é ' + s)
+
+var n1 = Number.parseFloat(window.prompt('Digie um numero'))
+var n2 = Number.parseFloat(window.prompt('Digite outro numero'))
+var s = n1 + n2
+var s = window.alert('A soma dos 2 numeros é ' + s)
+
+var n1 = Number(window.prompt('Digie um numero'))
+var n2 = Number(window.prompt('Digite outro numero'))
+var s = n1 + n2
+var s = window.alert('A soma dos 2 numeros é ' + s)
+
+
+
